3d8c066a096b79a4b549cb272d68a420a8d6ba92
fernandocostamabundadev <f0083357@gmail.com>
2026-07-10 14:44:12 +0200

    feat: criando escopo de rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,23 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+//Importar as rotas
+import todoRoutes from './router/todo.routes';
+
 dotenv.config();
 
 const app = express();
 
+//Middlewares
 app.use(cors());
 app.use(express.json());
 
+//rota teste
 app.get('/',(req, res)=>{
   res.status(200).json({message:'ok'})
 })
+
+//rota do todo
+app.use('/api/todo', todoRoutes);
 
 export default app;
---
+++ backend/src/router/todo.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const router = Router();
+
+//listar tarefa
+router.get('/',(req, res)=>{});
+//criar tarefa
+router.post('/',(req, res)=>{});
+//atualizar toda tarefa
+router.put('/:id',(req, res)=>{});
+//editar tarefa
+router.patch('/:id',(req, res)=>{});
+//eliminar tarefa
+router.delete('/:id',(req, res)=>{});
