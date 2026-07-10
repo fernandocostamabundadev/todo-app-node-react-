5517c5c618fce9767a01a1d1fac2b6426a1fd84b
fernandocostamabundadev <f0083357@gmail.com>
2026-07-10 16:32:50 +0200

    feat:criando schema de validacao com zod

--- backend/src/validations/todo.validation.js
+++ backend/src/validations/todo.validation.js
@@ -1,8 +1,25 @@
 import { z } from 'zod';
 
 //validar criacao da tarefa
-export const createTodoSchema = z.object({});
+export const createTodoSchema = z.object({
+  body: z.object({
+  title: z.string().min(1, 'Título é obrigatório'),
+  completed: z.boolean().optiona(),
+  })
+}),
 //valida atualizacao da tarefa
-export const updateTodoSchema = z.object({});
+export const updateTodoSchema = z.object({
+  body: z.object({
+    title: z.string().min(1, 'Título é obrigatório').optional(),
+    completed: z.boolean().optional(),
+  }),
+  params: z.object({
+    id: z.string().regex(/^\d+$/, 'ID deve ser um número'),
+  }),
+});
 //validar ID
-export const idParamSchema = z.object({})
+export const idParamSchema = z.object({
+  params: z.object({
+    id: z.string().regex(/^\d+$/, 'ID deve ser um número válido'),
+  }),
+})
