a1c9adff1fa56963a5c6ae0e440d2c34fe94ae64
fernandocostamabundadev <f0083357@gmail.com>
2026-07-10 15:02:48 +0200

    feat: criando estrutura base para banco de dados em memoria

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -1,3 +1,5 @@
+import todoService from '../services/todo.service';
+
 exports.gerAllTodo = (req, res, next)=>{};
 exports.createTodo= (req, res, next)=>{};
 exports.updateTodo= (req, res, next)=>{};
---
+++ backend/src/services/todo.service.js
@@ -0,0 +1,6 @@
+let todo =[
+  {id:1, title:'estudar html5', completred:true},
+  {id:2, title:'estudar css3', completred:false},
+  {id:3, title:'estudar react.js', completred:true},
+  {id:4, title:'estudar node.js', completred:false}
+]
